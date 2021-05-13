a74ab433f68cf8c21a06e8c51da02064afcb512f
Guilucas <guilhermehenriqueslucas@gmail.com>
2021-05-13 15:34:50 +0100

    using react-feather for svg icons

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '../core/button/Button';
+import { XCircle, CheckCircle } from 'react-feather';
 import styles from './Task.module.css';
 
 export default function Task(props) {
@@ -10,8 +11,18 @@ export default function Task(props) {
 			>
 				{props.task.content}
 			</span>
-			<Button onClick={() => props.removeTask(props.task)}>X</Button>
-			<Button onClick={() => props.updateTask(props.index)}>Complete</Button>
+			<Button
+				buttonType={'defaultButton'}
+				onClick={() => props.removeTask(props.task)}
+			>
+				<XCircle />
+			</Button>
+			<Button
+				buttonType={'defaultButton'}
+				onClick={() => props.updateTask(props.index)}
+			>
+				<CheckCircle />
+			</Button>
 		</div>
 	);
 }
