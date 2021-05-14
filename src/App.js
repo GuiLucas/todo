b642c21dddbbf67fe17cb55ae19d5ad2aae76e91
Guilucas <guilhermehenriqueslucas@gmail.com>
2021-05-14 17:32:47 +0100

    implemented header in app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import styles from './App.module.css';
 
 export default function App() {
 	return (
-		<main>
-			<AppController />
-		</main>
+		<>
+			<header>
+				<h1 className={styles.mainHeading}>TSKS</h1>
+				<p className={styles.mainP}>Organize your day better!</p>
+			</header>
+			<main className={styles.main}>
+				<AppController />
+			</main>
+		</>
 	);
 }
