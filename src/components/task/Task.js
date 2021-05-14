da038374cdd2e00e8159cd6ad71e59c9775d20c4
Guilucas <guilhermehenriqueslucas@gmail.com>
2021-05-14 17:34:24 +0100

    styling for task component

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -6,19 +6,27 @@ import styles from './Task.module.css';
 export default function Task(props) {
 	return (
 		<div className={styles.task}>
-			<span>{props.task.content}</span>
-			<Button
-				buttonType={'defaultButton'}
-				onClick={() => props.removeTask(props.task)}
+			<p
+				style={{
+					textDecoration: props.task.isCompleted ? 'line-through' : '',
+				}}
 			>
-				<XCircle />
-			</Button>
-			<Button
-				buttonType={'defaultButton'}
-				onClick={() => props.updateTask(props.task)}
-			>
-				<CheckCircle />
-			</Button>
+				{props.task.content}
+			</p>
+			<div className={styles.buttonGroup}>
+				<Button
+					buttonType={'defaultButton'}
+					onClick={() => props.removeTask(props.task)}
+				>
+					<XCircle className={styles.removeButton} />
+				</Button>
+				<Button
+					buttonType={'defaultButton'}
+					onClick={() => props.updateTask(props.task)}
+				>
+					<CheckCircle className={styles.completeButton} />
+				</Button>
+			</div>
 		</div>
 	);
 }
