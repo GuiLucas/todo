38e0590a2835944f70a58c260cff385488fd70c1
Guilucas <guilhermehenriqueslucas@gmail.com>
2021-05-17 15:52:44 +0100

    added footer and styling

--- src/App.js
+++ src/App.js
@@ -4,14 +4,28 @@ import styles from './App.module.css';
 
 export default function App() {
 	return (
-		<>
-			<header>
+		<div className={styles.appContainer}>
+			<header className={styles.header}>
 				<h1 className={styles.mainHeading}>TSKS</h1>
 				<p className={styles.mainP}>Organize your day better!</p>
 			</header>
 			<main className={styles.main}>
 				<AppController />
 			</main>
-		</>
+			<footer className={styles.footer}>
+				<p>
+					Made by{' '}
+					<a
+						className={styles.footerLink}
+						href='https://github.com/GuiLucas'
+						target='_blank'
+						rel='noreferrer'
+					>
+						Guilherme Lucas
+					</a>
+					.
+				</p>
+			</footer>
+		</div>
 	);
 }
