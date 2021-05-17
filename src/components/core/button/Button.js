40d247ae1108e8c6b691f681642d2492f6b66d23
Guilucas <guilhermehenriqueslucas@gmail.com>
2021-05-17 12:29:56 +0100

    fixed bug for className prop

--- src/components/core/button/Button.js
+++ src/components/core/button/Button.js
@@ -4,7 +4,7 @@ import styles from './Button.module.css';
 const Button = (props) => {
 	return (
 		<button
-			className={styles.defaultButton}
+			className={styles[props.buttonType]}
 			onClick={props.onClick}
 			disabled={props.disabled}
 		>
