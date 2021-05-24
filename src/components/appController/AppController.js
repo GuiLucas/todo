fa9a0a6cd7164772648ad2c313606b173815ee81
Guilucas <guilhermehenriqueslucas@gmail.com>
2021-05-24 13:54:12 +0100

    created reducer for state management

--- src/components/appController/AppController.js
+++ src/components/appController/AppController.js
@@ -1,74 +1,103 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import Container from '../core/container/Container';
 import FormContainer from '../formContainer/FormContainer';
 import Task from '../task/Task';
+import { v4 as uuidv4 } from 'uuid';
 
 // Styling
 import styles from './AppController.module.css';
 
+const initialState = { tasks: [], toRemove: [] };
+
+const taskReducer = (state, action) => {
+	switch (action.type) {
+		case 'ADD_TODO':
+			return {
+				id: uuidv4(),
+				content: action.content,
+				createdAt: new Date(),
+				isCompleted: false,
+				completedAt: null,
+			};
+		case 'TOGGLE_TODO':
+			if (state.id !== action.id) {
+				return state;
+			}
+
+			return {
+				...state,
+				isCompleted: !state.isCompleted,
+				completedAt: new Date(),
+			};
+		default:
+			return state;
+	}
+};
+
+const tasksReducer = (state = { tasks: [], toRemove: [] }, action) => {
+	switch (action.type) {
+		case 'ADD_TODO':
+			return {
+				...state,
+				tasks: [...state.tasks, taskReducer(undefined, action)],
+			};
+		case 'TOGGLE_TODO':
+			return {
+				...state,
+				tasks: state.tasks.map((t) => taskReducer(t, action)),
+			};
+		case 'REMOVE_TODO':
+			return {
+				...state,
+				tasks: state.tasks.filter((t) => t.id !== action.id),
+			};
+		case 'RESET':
+			return initialState;
+		default:
+			return state;
+	}
+};
+
 export default function AppController() {
-	const [currentTasks, setCurrentTasks] = useState([]);
-	const [completedTasks, setCompletedTasks] = useState([]);
+	const [state, dispatch] = useReducer(tasksReducer, initialState);
 
-	const addTask = (content) => {
-		const newTask = {
-			content: content,
-			createdAt: new Date(),
-			isCompleted: false,
-		};
-		setCurrentTasks([...currentTasks, newTask]);
-	};
+	const addTask = (content) => dispatch({ type: 'ADD_TODO', content: content });
 
-	const removeTaskFromCurrent = (task) => {
-		if (!task.isCompleted) {
-			setCurrentTasks(currentTasks.filter((result) => result !== task));
-		} else {
-			setCompletedTasks(completedTasks.filter((result) => result !== task));
-		}
-	};
+	const updateTask = (id) => dispatch({ type: 'TOGGLE_TODO', id: id });
 
-	const updateTaskCompleted = (task) => {
-		const newTask = {
-			...task,
-			isCompleted: true,
-			completedAt: new Date(),
-		};
-		setCompletedTasks([...completedTasks, newTask]);
-		removeTaskFromCurrent(task);
-	};
+	const removeTask = (id) => dispatch({ type: 'REMOVE_TODO', id: id });
 
-	let currentList = null;
+	// const resetState = () => dispatch({ type: 'RESET' });
 
-	if (currentTasks) {
-		currentList =
-			currentTasks &&
-			currentTasks.map((task, index) => {
-				return (
-					<Task
-						key={index}
-						task={task}
-						updateTask={updateTaskCompleted}
-						removeTask={removeTaskFromCurrent}
-					/>
-				);
-			});
-	}
+	const currentList = state.tasks
+		.filter((task) => !task.isCompleted)
+		.map((task) => {
+			return (
+				<Task
+					key={task.id}
+					{...task}
+					updateTask={() => updateTask(task.id)}
+					removeTask={() => removeTask(task.id)}
+				/>
+			);
+		});
 
-	let completedList = null;
-
-	if (completedTasks) {
-		completedList =
-			completedTasks &&
-			completedTasks.map((task, index) => {
-				return (
-					<Task key={index} task={task} removeTask={removeTaskFromCurrent} />
-				);
-			});
-	}
+	const completedList = state.tasks
+		.filter((task) => task.isCompleted)
+		.map((task) => {
+			return (
+				<Task
+					key={task.id}
+					{...task}
+					updateTask={() => updateTask(task.id)}
+					removeTask={() => removeTask(task.id)}
+				/>
+			);
+		});
 
 	return (
 		<Container>
-			<FormContainer addTask={addTask} />
+			<FormContainer addTask={(inputValue) => addTask(inputValue)} />
 
 			{currentList.length > 0 ? (
 				<div className={styles.tasksContainer}>{currentList}</div>
